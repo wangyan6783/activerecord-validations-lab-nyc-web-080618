76bd04061ace5c741821ae3ad817368f241aa688
Yan Wang <Yan@Flatirons-Air-8.fios-router.home>
2018-09-02 14:45:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbaity
+
+
+  def is_clickbaity
+    if title == nil || word_check == false
+      errors.add(:title, "not clickbaity")
+    end
+  end
+
+
+  def word_check
+    valid_words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+    valid_words.each do |word|
+      if title.include?(word)
+        return true
+      end
+    end
+    false
+  end
 end
